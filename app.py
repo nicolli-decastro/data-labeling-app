05825e6b18833b3da321d1c0b32f7d94a31a5c33
Nicolli Decastro <nicolli.decastro@gmail.com>
2025-06-03 17:22:41 -0400

    Unique drive_folder_id

--- app.py
+++ app.py
@@ -95,10 +95,10 @@ else:
                         elif labeled_df.empty:
                             if st.button("🚀 Start Labeling", key=key):
                                 if not drive_folder_id:
-                                    new_drive_folder_id = du.create_drive_folder(folder_name, drive_folder_id)
+                                    drive_folder_id = du.create_drive_folder(folder_name, drive_folder_id)
                                 df_original[['listing_url', 'photo_url', 'price', 'title', 'location', 'origin_city_list']].assign(
                                     user_name='', binary_flag='', timestamp='').to_csv("temp.csv", index=False)
-                                du.upload_csv(pd.read_csv("temp.csv"), file, new_drive_folder_id)
+                                du.upload_csv(pd.read_csv("temp.csv"), file, drive_folder_id)
                                 st.session_state.selected_dataset = {
                                     "csv_path": csv_path,
                                     "images_folder": images_folder,
@@ -106,7 +106,7 @@ else:
                                     "location": location,
                                     "range": range_miles,
                                     "drive_file": file,
-                                    "drive_folder_id": new_drive_folder_id
+                                    "drive_folder_id": drive_folder_id
                                 }
                                 st.rerun()
                         else:
